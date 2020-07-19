56c6cac22923677da7d59a4c06b15f2516b1ad7c
Saleem Saleh <salem1234saleem@gmail.com>
2020-07-19 14:10:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
+
 // Add your code here
+
+let submit = document.getElementById("submit");
+submit.addEventListener("submit", (e) => {
+       e.preventDefault();
+      let name = document.getElementById("name").innerHTML;
+      let name = document.getElementById("email").innerHTML;
+      submitData(name ,email);
+});
+
+
+function submitData(name ,email){
+
+      let formData = {
+      dogName: name,
+      dogBreed: email
+      };
+
+      let configObj = {
+      method: "POST",
+      headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+      };
+
+      fetch("http://localhost:3000/users", configObj)
+      .then(function(response) {
+      return response.json();
+      })
+      .then(function(object) {
+          respondFun(object);
+      }).catch(function(error) {
+         alert("Bad things! Ragnarők!");
+        console.log(error.message);
+  });
+}
+
+function respondFun(object){
+   document.getElementById("name").innerHTML = object ;
+}
